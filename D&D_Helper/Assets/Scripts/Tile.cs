5cd586dc532b5c067ed0343dd1c99dd5e1f4d51d
Rainy-Day-Development <35881097+Rainy-Day-Development@users.noreply.github.com>
2018-02-03 17:51:03 -0800

    Kris
    Bit of movement stuff

--- D&D_Helper/Assets/Scripts/Tile.cs
+++ D&D_Helper/Assets/Scripts/Tile.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour {
 
+    public bool Current = false;
+    public bool Target = false;
+    public bool Selectable = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,24 @@ public class Tile : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Current)
+        {
+            GetComponent<Renderer>().material.color = Color.magenta;
+        }
+        else if (Target)
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        else if (Selectable)
+        {
+            GetComponent<Renderer>().material.color = Color.cyan;
+        }
+        else {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+
+
+
+
 	}
 }
