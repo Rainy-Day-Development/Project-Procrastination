1525f4ad4af1b70176be3d8865b8fd749359242f
Rainy-Day-Development <35881097+Rainy-Day-Development@users.noreply.github.com>
2018-02-16 23:29:48 -0800

    Revert "Initial commit"
    This reverts commit 375f12f586ea971860d04edb6c6e874172939996.

--- D&D_Helper/Assets/Scripts/TacticsCamera.cs
+++ D&D_Helper/Assets/Scripts/TacticsCamera.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class TacticsCamera : MonoBehaviour {
 
+    public Vector3 MapPos;
+    public GameObject Map;
+
+    void Start()
+    {
+        Map = GameObject.FindGameObjectWithTag("Map");
+        MapPos = new Vector3(Map.transform.position.x, Map.transform.position.y, Map.transform.position.z);
+        Debug.Log("x = " + MapPos.x);
+        transform.localPosition = new Vector3(MapPos.x, MapPos.y, MapPos.z);
+        Debug.Log("x = " + this.transform.position.x);
+    }
+
     public void RotateLeft() {
         transform.Rotate(Vector3.up, 90, Space.Self);
     }
